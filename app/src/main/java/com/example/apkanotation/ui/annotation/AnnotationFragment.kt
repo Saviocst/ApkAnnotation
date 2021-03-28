9deb9f065c78dbe7e44652a8e22f6622da1bf136
Sávio Da Costa Santos <saviocosta.flam20@gmail.com>
2021-03-27 21:23:41 -0300

    evento de input

--- app/src/main/java/com/example/apkanotation/ui/annotation/AnnotationFragment.kt
+++ app/src/main/java/com/example/apkanotation/ui/annotation/AnnotationFragment.kt
@@ -2,21 +2,82 @@ package com.example.apkanotation.ui.annotation
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.apkanotation.R
+import com.example.apkanotation.data.db.DataBaseSv
+import com.example.apkanotation.data.db.dao.AnnotationDAO
+import com.example.apkanotation.extention.hideKeyBoard
+import com.example.apkanotation.repository.AnnotationRepository
+import com.example.apkanotation.repository.DataBaseDataSource
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.annotation_fragment.*
 
-class AnnotationFragment : Fragment() {
+class AnnotationFragment : Fragment(R.layout.annotation_fragment) {
 
-    private lateinit var viewModel: AnnotationViewModel
+//lateinit var inputName: EditText
+//lateinit var inputRemember: EditText
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.annotation_fragment, container, false)
+    private val viewModel: AnnotationViewModel by viewModels {
+        object : ViewModelProvider.Factory{
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+
+                val annotationDAO: AnnotationDAO = DataBaseSv.getInstance(requireContext()).annotationDAO
+
+                val repository: AnnotationRepository = DataBaseDataSource(annotationDAO)
+                return AnnotationViewModel(repository) as T
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeEvents()
+        setListeners()
     }
 
 
+    private fun observeEvents() {
+        viewModel.annotationStateEventData.observe(viewLifecycleOwner){annotationState ->
+            when (annotationState) {
+                is AnnotationViewModel.AnnotationState.Inserted -> {
+                    clearFields()
+                    hideKeyBoard()
+                }
+
+            }
+        }
+
+        viewModel.messageEventData.observe(viewLifecycleOwner) {stringResId ->
+            Snackbar.make(requireView(), stringResId, Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+
+    private fun clearFields() {
+        input_name.text?.clear()
+        input_remember.text?.clear()
+    }
+
+    private fun hideKeyBoard() {
+        val parentActivity = requireActivity()
+        if (parentActivity is AppCompatActivity){
+            parentActivity.hideKeyBoard() //Obs Estudar
+        }
+    }
+
+    private fun setListeners() {
+        btn_adicionar.setOnClickListener {
+            val name = input_name.text.toString()
+            val annotation = input_name.text.toString()
+            viewModel.addAnnotation(name, annotation)
+        }
+    }
+
+    // Video 7 -> Continuar
 }
--- app/src/main/java/com/example/apkanotation/ui/annotation/AnnotationViewModel.kt
+++ app/src/main/java/com/example/apkanotation/ui/annotation/AnnotationViewModel.kt
@@ -1,7 +1,44 @@
 package com.example.apkanotation.ui.annotation
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.apkanotation.R
+import com.example.apkanotation.repository.AnnotationRepository
+import kotlinx.coroutines.launch
 
-class AnnotationViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class AnnotationViewModel(private val repository: AnnotationRepository) : ViewModel() {
+
+    private val _annotationStateEventData = MutableLiveData<AnnotationState>()
+    val annotationStateEventData: LiveData<AnnotationState>
+    get() = _annotationStateEventData
+
+    private val _messageEventData = MutableLiveData<Int>()
+    val messageEventData: LiveData<Int>
+    get() = _messageEventData
+
+    fun addAnnotation(name: String, annotation: String) = viewModelScope.launch {
+
+        try {
+            val id = repository.insertAnnotation(name, annotation)
+            if (id > 0){
+                _annotationStateEventData.value = AnnotationState.Inserted
+                _messageEventData.value = R.string.annotation_inserted
+            }
+        }catch (aviso: Exception){
+            _messageEventData.value = R.string.annotation_error
+            Log.e(TAG, aviso.toString())
+        }
+    }
+
+    sealed class AnnotationState{
+        object Inserted: AnnotationState()
+    }
+
+    companion object{
+        private val TAG = AnnotationViewModel::class.java.simpleName
+    }
+
 }
